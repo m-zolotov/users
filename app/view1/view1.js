a574f8b1b86e0e5140df4afa5475f5055305d611
Mikhail V. Zolotov <mvzolotov@tsc.ts>
2017-05-11 17:00:47 +0300

    users-list http get correct

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,15 +1,15 @@
 'use strict';
 
-angular.module('firstPage', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
-}]);
+}])
 
-firstPage.controller('View1Ctrl', ['$scope','$http', function($scope, $http) {
+.controller('View1Ctrl', ['$scope','$http', function($scope, $http, myUserService) {
     $scope.title = 'Список юзеров';
     $http.get('./api/users.json').success(function (data, status, headers, config) {
         $scope.users = data;
