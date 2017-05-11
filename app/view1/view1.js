3c445a7250d469b337eb2e03d87a9c63d85fd120
Mikhail V. Zolotov <mvzolotov@tsc.ts>
2017-05-11 16:44:12 +0300

    users-list http get

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,17 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('firstPage', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
-}])
+}]);
 
-.controller('View1Ctrl', [function() {
-
+firstPage.controller('View1Ctrl', ['$scope','$http', function($scope, $http) {
+    $scope.title = 'Список юзеров';
+    $http.get('./api/users.json').success(function (data, status, headers, config) {
+        $scope.users = data;
+    });
 }]);
