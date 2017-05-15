ecf20e6c7a97b50b34cf9bccc51a9522c240a3ca
Mikhail V. Zolotov <mvzolotov@tsc.ts>
2017-05-15 15:08:28 +0300

    factory user add if 3

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,40 +12,43 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .factory('UserService', function($http, $q){
     var userList = undefined;
-    return{
-        getData: function(){
-            var deferred = $q.defer();
+    if (userList === undefined) {
+        return{
+            getData: function(){
+                var deferred = $q.defer();
 
-            $http({
-                method: 'GET', url: './api/users.json'
-            }).
-            then (function success(response) {
-                    userList = response.data;
-                    deferred.resolve(userList);
-                },function error(response) {
-                    deferred.reject(response.status);
-                }
-            );
+                $http({
+                    method: 'GET', url: './api/users.json'
+                }).
+                then (function success(response) {
+                        userList = response.data;
+                        deferred.resolve(userList);
+                    },function error(response) {
+                        deferred.reject(response.status);
+                    }
+                );
 
-            return deferred.promise;
+                return deferred.promise;
+            }
         }
+    } else {
+        return userList;
     }
 })
 
 .controller('View1Ctrl', ['$scope', '$http', '$q', 'UserService', function($scope, $http, $q, UserService) {
     $scope.title = 'Список юзеров';
-    $scope.users = undefined;
-
     var promiseObj = UserService.getData();
+    /*promiseObj.then(function(value) {
+        $scope.users = value;
+    });*/
 
     promiseObj.then(function(value) {
-        if ($scope.users === undefined) {
-            $scope.users = value;
-            console.log ('users', $scope.users);
-            UserService.getData().then(function(value) {
-                $scope.users2 = value;
-                console.log ('users2', $scope.users2);
-            });
-        }
+        $scope.users = value;
+        console.log ('users', $scope.users);
+        UserService.getData().then(function(value) {
+            $scope.users2 = value;
+            console.log ('users2', $scope.users2);
+        });
     });
 }]);
