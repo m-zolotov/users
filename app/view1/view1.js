05f52ca1970c3ce69f3bf95010dfd25c41541e8f
Mikhail V. Zolotov <mvzolotov@tsc.ts>
2017-05-15 11:57:43 +0300

    service 4

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,12 +11,17 @@ angular.module('myApp.view1', ['ngRoute'])
 
 
 .factory('UserService', function($http, $q){
+    var userList = null;
     return{
         getData: function(){
             var deferred = $q.defer();
-            $http({method: 'GET', url: './api/users.json'}).
+            $http({
+                method: 'GET', url: './api/users.json'
+            }).
             then (function success(response) {
-                    deferred.resolve(response.data.question);
+                    /*deferred.resolve(response.data.question);*/
+                    userList = response.data.question;
+                    deferred.resolve(userList);
                 },function error(response) {
                     deferred.reject(response.status);
                 }
@@ -27,52 +32,55 @@ angular.module('myApp.view1', ['ngRoute'])
     }
 })
 
-/*.factory('UserService', function($http, $q){
-    return{
-        getData: function(){
-            var deferred = $q.defer();
-            $http({method: 'GET', url: './api/users.json'}).
-            then (function success(response) {
-                    deferred.resolve(response.data.question);
-                },function error(response) {
-                    deferred.reject(response.status);
-                }
-            );
-            console.log(deferred.promise);
-            return deferred.promise;
-        }
-    }
-})*/
-
-
-/*.service('UserService', function ($http) {
-    var self = this;
-    this.userList = '';
-    $http.get('./api/users.json').success(function (data, status, headers, config) {
-        self.userList = data;
-        console.log(self.userList);
-    });
-})*/
-
 .controller('View1Ctrl', ['$scope', '$http', '$q', 'UserService', function($scope, $http, $q, UserService) {
     $scope.title = 'Список юзеров';
-    /*$scope.users = UserService;
-    console.log(UserService);
-    $q*/
-    /*$scope.users = UserService.getData();*/
-    /*var promiseObj=UserService.getData();
+
+    var promiseObj=UserService.getData();
     promiseObj.then(function(value) {
         $scope.users = value;
     });
-    console.log($scope.users);*/
-
-    var promiseObj=UserService.getData();
-    promiseObj.then(function(value) { $scope.question=value; });
+    console.log($scope.users);
+    
+    /*$scope.users = UserService;
+     console.log(UserService);
+     $q*/
+    /*$scope.users = UserService.getData();*/
+    /*var promiseObj = UserService.getData();
+    promiseObj.then(function(value) {
+        $scope.question=value;
+    });
 
     $scope.voteUp = function (answer){
         answer.rate++;
     };
     $scope.voteDown = function (answer){
         answer.rate--;
-    };
+    };*/
 }]);
+
+/*.factory('UserService', function($http, $q){
+ return{
+ getData: function(){
+ var deferred = $q.defer();
+ $http({method: 'GET', url: './api/users.json'}).
+ then (function success(response) {
+ deferred.resolve(response.data.question);
+ },function error(response) {
+ deferred.reject(response.status);
+ }
+ );
+ console.log(deferred.promise);
+ return deferred.promise;
+ }
+ }
+ })*/
+
+
+/*.service('UserService', function ($http) {
+ var self = this;
+ this.userList = '';
+ $http.get('./api/users.json').success(function (data, status, headers, config) {
+ self.userList = data;
+ console.log(self.userList);
+ });
+ })*/
