9d850d80f347b2b337eaca2ba9869010df703fc8
m-zolotov <mvzolotov@tsft.ru>
2017-05-17 17:11:30 +0300

    Users-list component

--- app/users/users.component.js
+++ app/users/users.component.js
@@ -11,4 +11,15 @@ angular.module('Users')
                 self.users = value;
             });
         }]
-    });
+    })
+    /*.component('userDetail', {
+        templateUrl: 'users/users-detail.template.html',
+        controller: ['userService', '$http', '$q', function(userService, $http, $q) {
+            var self = this;
+            var promiseObj = userService.getData();
+            self.title = 'Список юзеров';
+            promiseObj.then(function(value) {
+                self.users = value;
+            });
+        }]
+    })*/;
