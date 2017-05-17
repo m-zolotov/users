2b6d37e2501e08f744b71b18ebaaa24d1641ea62
Mikhail V. Zolotov <mvzolotov@tsc.ts>
2017-05-17 16:30:11 +0300

    true component

--- app/users/users.component.js
+++ app/users/users.component.js
@@ -1,11 +1,14 @@
 'use strict';
 
 angular.module('Users')
-    .controller('UsersListCtr', ['$http', '$q', 'userService', function($http, $q, userService) {
-        var self = this;
-        var promiseObj = userService.getData();
-        self.title = 'Список юзеров';
-        promiseObj.then(function(value) {
-            self.users = value;
-        });
-    }]);
+    .component('usersList', {
+        templateUrl: 'users/users-list.template.html',
+        controller: ['userService', '$http', '$q', function(userService, $http, $q) {
+            var self = this;
+            var promiseObj = userService.getData();
+            self.title = 'Список юзеров';
+            promiseObj.then(function(value) {
+                self.users = value;
+            });
+        }]
+    });
