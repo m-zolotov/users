cb267a8abb54048a102b4be0a286b7013421b172
m-zolotov <mvzolotov@tsft.ru>
2017-05-23 15:29:42 +0300

    userDetail 1

--- app/users/users-details.component.js
+++ app/users/users-details.component.js
@@ -5,10 +5,19 @@ angular.module('Users')
         templateUrl: 'users/users-detail.template.html',
         controller: ['userService', '$http', '$q', '$routeParams', function(userService, $http, $q, $routeParams) {
             var self = this;
+            var userProfile;
             var userID = $routeParams.userId;
             var promiseObjDetail = userService.getData();
-            console.log(promiseObjDetail);
-            //var userName = $routeParams.userName;
-            self.title = 'Сраница юзера ' + userID;//user.name
+            promiseObjDetail.then(function(value) {
+                for (var i = 0; i < value.length; i++) {
+                    if (value[i].id === userID) {
+                        userProfile = value[i];
+                    }
+                }
+            });
+            console.log(userProfile);
+
+            //self.title = userID;//user.name;
+
         }]
     });
