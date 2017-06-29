1b59fb7a5d99b0090f8c62d0ab387dd10dab0d60
m-zolotov <mvzolotov@tsft.ru>
2017-06-29 17:23:37 +0300

    Product Menu 0.3

--- app/accordion/product-accordion.component.js
+++ app/accordion/product-accordion.component.js
@@ -6,17 +6,8 @@ angular.module('Accordion')
         controller: ['accordionService', '$http', '$q', '$routeParams', function(accordionService, $http, $q, $routeParams) {
             var self = this;
             self.title = 'Список продуктов';
-            self.hideElement = true;
-            self.showElement = false;
-
-            self.visibleElements = function () {
-                if (self.hideElement === true && self.showElement === false) {
-                    self.hideElement = false;
-                    self.showElement = true;
-                } else if (self.hideElement === false && self.showElement === true) {
-                    self.hideElement = true;
-                    self.showElement = false;
-                }
+            self.template = {
+                url: '/accordion/product-category.html'
             };
 
             accordionService.getData().then(function(value) {
