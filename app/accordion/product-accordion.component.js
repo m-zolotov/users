268949041bbd03342dcdcf87dc085f9c93014738
m-zolotov <mvzolotov@tsft.ru>
2017-07-04 13:41:02 +0300

    Product Menu 0.4

--- app/accordion/product-accordion.component.js
+++ app/accordion/product-accordion.component.js
@@ -5,6 +5,7 @@ angular.module('Accordion')
         templateUrl: 'accordion/product-accordion.template.html',
         controller: ['accordionService', '$http', '$q', '$routeParams', function(accordionService, $http, $q, $routeParams) {
             var self = this;
+
             self.title = 'Список продуктов';
             self.template = {
                 url: '/accordion/product-category.html'
@@ -13,5 +14,52 @@ angular.module('Accordion')
             accordionService.getData().then(function(value) {
                 self.products = value;
             });
+
+            self.closeTooltip = function (allProducts) {
+                var items;
+                for (var i = 0; i < allProducts.length; i++) {
+                    allProducts[i].tooltipOpen = "false";
+                    if (typeof(allProducts[i].products) === 'object') {
+                        items = allProducts[i].products;
+                        self.setTooltip(items);
+                    }
+                }
+            };
+
+            self.setTooltip = function(product) {
+                var items;
+                /*console.log('product', product);
+                console.log('tooltipOpen', typeof(product.tooltipOpen));*/
+                if (product.tooltipOpen === "false") {
+                    console.log('tooltipOpen', product);
+                    self.closeTooltip(self.products);
+                    product.tooltipOpen = "true";
+                } else if (product.tooltipOpen === "true") {
+                    console.log('tooltipOpen', product);
+                    self.closeTooltip(self.products);
+                }
+            }
         }]
     });
+/*if (product.tooltipOpen === "false") {
+ for (var i = 0; i < self.products.length; i++) {
+ self.products[i].tooltipOpen = "false";
+ console.log('self.products[i]', self.products[i]);
+ if (typeof(self.products[i].products) === 'object') {
+ items = self.products[i].products;
+ self.setTooltip(items);
+ //debugger;
+ console.log('self.products[i].products', self.products[i].products);
+ }
+ }
+ product.tooltipOpen = "true";
+ } else if (product.tooltipOpen === "true") {
+ for (var i = 0; i < self.products.length; i++) {
+ self.products[i].tooltipOpen = "false";
+ if (typeof(self.products[i].products) === 'object') {
+ items = self.products[i].products;
+ self.setTooltip(items);
+ //debugger;
+ }
+ }
+ }*/
