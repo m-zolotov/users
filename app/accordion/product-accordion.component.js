124c3d5416fdc5d076680d0ea94b63228ca6a8bd
m-zolotov <mvzolotov@tsft.ru>
2017-07-04 15:22:54 +0300

    Product Menu 0.5

--- app/accordion/product-accordion.component.js
+++ app/accordion/product-accordion.component.js
@@ -18,48 +18,24 @@ angular.module('Accordion')
             self.closeTooltip = function (allProducts) {
                 var items;
                 for (var i = 0; i < allProducts.length; i++) {
-                    allProducts[i].tooltipOpen = "false";
+                    allProducts[i].tooltipOpen = false;
                     if (typeof(allProducts[i].products) === 'object') {
                         items = allProducts[i].products;
-                        self.setTooltip(items);
+                        self.closeTooltip(items);
                     }
                 }
             };
 
             self.setTooltip = function(product) {
                 var items;
-                /*console.log('product', product);
-                console.log('tooltipOpen', typeof(product.tooltipOpen));*/
-                if (product.tooltipOpen === "false") {
+                if (product.tooltipOpen === false) {
                     console.log('tooltipOpen', product);
                     self.closeTooltip(self.products);
-                    product.tooltipOpen = "true";
-                } else if (product.tooltipOpen === "true") {
+                    product.tooltipOpen = true;
+                } else if (product.tooltipOpen === true) {
                     console.log('tooltipOpen', product);
                     self.closeTooltip(self.products);
                 }
             }
         }]
     });
-/*if (product.tooltipOpen === "false") {
- for (var i = 0; i < self.products.length; i++) {
- self.products[i].tooltipOpen = "false";
- console.log('self.products[i]', self.products[i]);
- if (typeof(self.products[i].products) === 'object') {
- items = self.products[i].products;
- self.setTooltip(items);
- //debugger;
- console.log('self.products[i].products', self.products[i].products);
- }
- }
- product.tooltipOpen = "true";
- } else if (product.tooltipOpen === "true") {
- for (var i = 0; i < self.products.length; i++) {
- self.products[i].tooltipOpen = "false";
- if (typeof(self.products[i].products) === 'object') {
- items = self.products[i].products;
- self.setTooltip(items);
- //debugger;
- }
- }
- }*/
